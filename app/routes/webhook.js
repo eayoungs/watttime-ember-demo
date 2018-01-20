bcd77e72b138b91ab4898fbc18c0753134ad1e22
Eric Youngson <eayoungs@gmail.com>
2018-01-20 06:24:11 -0800

    Moved from static data to API call

--- app/routes/webhook.js
+++ app/routes/webhook.js
@@ -1,14 +1,14 @@
+import fetch from 'fetch';
 import Route from '@ember/routing/route';
 
+
 export default Route.extend({
-    model() {
-        return [
-            {
-            "timestamp": "2018-01-01T08:00:00Z",
-            "created_at": "2017-12-31T17:05:20.098287Z",
-            "carbon": 1482.75096836669,
-            "genmix": [{"fuel": "other"}]
-            }
-        ]
-    }
+  model() {
+    return fetch("https://api.watttime.org/api/v1/datapoints/17969688/")
+      .then(
+        function(response) {
+          return response.json();
+        }
+      );
+  }
 });
